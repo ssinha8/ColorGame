f7e796a617407248385ca66a9284068b44276945
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2022-05-08 22:09:48 -0700

    added keys

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,10 +11,20 @@ class Play extends Phaser.Scene {
 
   // Does nothing right now
   create() {
+
+    //Set up keys
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
+
+    //Add gameplay objects
     this.player = new plChr(this, 300, 300, 'placeholder', 0);
   }
 
   
   update() {
+    this.player.update();
   }
 }
