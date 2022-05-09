135cf117000c5f0d0f10247f925b0a52efb38633
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2022-05-08 21:45:03 -0700

    add main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 960, //TODO: finalize width/height
+    height: 540, // and see if we can fix the bug that doubles the size
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [Menu, Play] // Shop scene has been scrapped due to lack of time.
+}
+
+let keyUP, keyDOWN, keyLEFT, keyRIGHT, keyCONFIRM, keyJUMP, keyCANCEL, keyPAUSE;
+let game = new Phaser.Game(config);
