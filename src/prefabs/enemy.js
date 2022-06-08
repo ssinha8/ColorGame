b0579e297b56d086b8cbf4a0168a54acb7e80ef6
AnnaSaysHi <headsilosetailsyouwin@gmail.com>
2022-06-07 19:11:01 -0700

    made enemies move

--- src/prefabs/enemy.js
+++ src/prefabs/enemy.js
@@ -1,12 +1,16 @@
 class enemy extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y, texture, frame){
+    constructor(scene, x, y, texture, frame, direction){
         super(scene, x, y, texture, frame);
+        this.direction = direction;
         scene.add.existing(this);
         scene.physics.add.existing(this);
+        this.setAccelerationY(1750);
+        this.body.setMaxVelocityY(1000);
+        this.speedConst = 400;
     }
 
     update(){
-        //TODO
+        this.body.setVelocityX(speedConst * direction);
     }
 
     reset(){
