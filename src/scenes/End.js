709e327074406e22341f6dbdc9df1272cf5441ba
nicoletteL <nantisde@ucsc.edu>
2022-06-08 00:16:05 -0700

    Modified endscreen to fit

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -9,5 +9,7 @@ class End extends Phaser.Scene {
 
     create() {
         const win = this.add.tileSprite(0, 0, this.game.width, this.game.height, 'win').setOrigin(0,0);
+        win.setScaleX(0.5);
+        win.setScaleY(544/1280);
     }
 }
